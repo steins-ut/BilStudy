56262353021ad19fde6ee658dafd2e2f69970d9a
Orhun Ege Çelik <77338444+oegecelik@users.noreply.github.com>
2023-05-27 16:25:03 +0300

    Merge branch 'master' of C:\Users\oegec\Documents\GitHub\BilStudy with conflicts.

---
+++ app/src/main/java/com/merko/bilstudy/MindMapActivity.java
@@ -0,0 +1,14 @@
+package com.merko.bilstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MindMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mind_map);
+    }
+}
--- app/src/main/java/com/merko/bilstudy/MindMapTemplatesActivity.java
+++ app/src/main/java/com/merko/bilstudy/MindMapTemplatesActivity.java
@@ -8,16 +8,25 @@ import android.view.View;
 
 public class MindMapTemplatesActivity extends AppCompatActivity {
     private View back;
+    private View normalMM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mind_map_templates);
         back = findViewById(R.id.back_button);
+        normalMM = findViewById(R.id.bubble_maps);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MindMapTemplatesActivity.this, ChooseTemplateActivity.class);
+                startActivity(intent);
+            }
+        });
+        normalMM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MindMapTemplatesActivity.this, MindMapActivity.class);
                 startActivity(intent);
             }
         });
